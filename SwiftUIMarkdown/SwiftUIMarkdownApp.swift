98412a656bc08ff3cd59336f9a5ec3ccf9cb1541
D. Prameswara <meshwara@gmail.com>
2023-09-24 05:19:53 +0700

    Initial Commit

---
+++ SwiftUIMarkdown/SwiftUIMarkdownApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIMarkdownApp.swift
+//  SwiftUIMarkdown
+//
+//  Created by D. Prameswara on 24/09/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIMarkdownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
